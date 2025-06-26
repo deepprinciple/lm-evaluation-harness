a7cd3262eeafd7d0dbaa5d592be19bf8094368bb
iamthomaspruyn <tompruyn84@gmail.com>
2025-06-26 19:16:34 -0400

    computational materials QandA set

--- lm_eval/tasks/science/advanced_reasoning/materials/utils.py
+++ lm_eval/tasks/science/advanced_reasoning/materials/utils.py
@@ -22,3 +22,21 @@ def process_polymer_tg(dataset):
         "question": f"SMILES: {x['Question'].split(',')[0].strip()} \n Density: {x['Question'].split(',')[1].strip()} \n",
         "answer": str(x["Answer"])
     })
+
+def process_lammps_vasp(dataset):
+    def format_row(row):
+        return {
+            "Question": (row.get("Question") or "").strip(),
+            "Option_A": (row.get("Option_A") or "").strip(),
+            "Option_B": (row.get("Option_B") or "").strip(),
+            "Option_C": (row.get("Option_C") or "").strip(),
+            "Option_D": (row.get("Option_D") or "").strip(),
+            "Option_E": (row.get("Option_E") or "").strip(),
+            "Option_F": (row.get("Option_F") or "").strip(),
+            "Answer": (row.get("Answer") or "").strip(),
+            "Comment": (row.get("Comment") or "").strip(),  # VASP / LAMMPS
+        }
+    return dataset.map(format_row)
+
+
+
