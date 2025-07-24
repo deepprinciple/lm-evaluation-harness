6bd0a02957ce90d804d29922ea7bfbb3ebf7d31d
Jocelyn Lu <lujieyu@deepprinciple.com>
2025-07-24 20:05:23 +0800

    chemistry qa refactor

---
+++ lm_eval/tasks/science/advanced_reasoning/chemistry/utils.py
@@ -0,0 +1,11 @@
+from functools import partial
+
+def process_docs(dataset, task):
+    """Filter dataset by specific task type"""
+    return dataset.filter(lambda x: x["Task"] == task)
+
+# ['Forward Reaction Prediction', 'Retrosynthesis', 'Experimental Techniques', 'Molecular Property']
+process_forward_reaction_prediction = partial(process_docs, task='Forward Reaction Prediction')
+process_retrosynthesis = partial(process_docs, task='Retrosynthesis')
+process_experimental_techniques = partial(process_docs, task='Experimental Techniques')
+process_molecular_property = partial(process_docs, task='Molecular Property')
