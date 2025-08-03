05e922f3c9a18242e0d889600e0d0dcc5122ffe9
Jocelyn Lu <jl8570@nyu.edu>
2025-08-03 21:02:09 +0800

    update structure_elucidation

--- lm_eval/tasks/science/advanced_reasoning/chemistry/utils.py
+++ lm_eval/tasks/science/advanced_reasoning/chemistry/utils.py
@@ -3,8 +3,18 @@ Utility functions for processing evaluation datasets.
 """
 
 import re
-from typing import Dict, Any, List
+from typing import Dict, List
+from functools import partial
 
+def process_docs(dataset, task):
+    """Filter dataset by specific task type"""
+    return dataset.filter(lambda x: x["Task"] == task)
+
+process_forward_reaction_prediction = partial(process_docs, task='Forward Reaction Prediction')
+process_retrosynthesis = partial(process_docs, task='Retrosynthesis')
+process_experimental_techniques = partial(process_docs, task='Experimental Techniques')
+process_molecular_property = partial(process_docs, task='Molecular Property')
+process_quantum_software_usage = partial(process_docs, task='Quantum Software Usage')
 
 def process_docs(dataset: List[Dict]) -> List[Dict]:
     """
